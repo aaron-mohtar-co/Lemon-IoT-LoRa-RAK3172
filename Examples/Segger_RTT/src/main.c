5af5a43f4b73f46d9228e5ee7ecc4cbed2dcad8b
Craig Peacock <craigp@beyondlogic.org>
2023-07-28 16:02:43 +0930

    Segger RTT (Real Time Transfer) Example

---
+++ Examples/Segger_RTT/src/main.c
@@ -0,0 +1,26 @@
+/*
+ * Zephyr Hello World Boiler Plate Example
+ * Demonstrates a basic Zephyr project with console support
+ * 
+ * Copyright (c) 2023 Aaron Mohtar & Co Pty Ltd
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <zephyr/device.h>
+#include <zephyr/kernel.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(rtt);
+
+void main(void)
+{
+	printk("Zephyr Hello World! Example \nBoard: %s\n", CONFIG_BOARD);
+
+	while (1) {
+		printk("Hello World\n");
+		LOG_INF("Hello World");
+		k_msleep(1000);
+	}
+}
